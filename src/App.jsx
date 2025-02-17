38fc8363dd08f3b9fd6fb3e18dc48ba38540a11e
suyeon-dev <choilynne.dev@gmail.com>
2025-02-17 11:23:54 +0900

    fix:ESLint에러 해결 - App.jsx 라우터 중복 import 수정 - 헤더 import문 수정 - issue #7

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Index from './pages/mypage/Index';
 import EditProfile from './pages/mypage/EditProfile';
-
-import { Route, Routes } from 'react-router-dom';
 import Header from './components/common/Header';
 import Home from './pages/Index';
 import Purchase from './pages/purchase/Index';

757a347f4ec4dde8e0cf444a25c0dd50b212d6b5
suyeon-dev <choilynne.dev@gmail.com>
2025-02-19 00:40:11 +0900

    feat:Redux 누락 파일 추가[#18]

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+import authReducer from './reducers/authReducer';
+import productReducer from './reducers/productReducer';
+import chatReducer from './reducers/chatReducer';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  product: productReducer,
+  chats: chatReducer,
+  // 전역 관리 state가 추가되면 여기에 추가
+});
+
+export default rootReducer;

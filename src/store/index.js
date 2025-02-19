3c3660b11f90141e1941cba1dadccaf1fc0aaf87
suyeon-dev <choilynne.dev@gmail.com>
2025-02-19 11:25:44 +0900

    feat:Redux폴더 정리[#18]

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,6 @@
 import { combineReducers } from 'redux';
-import authReducer from './reducers/authReducer';
-import productReducer from './reducers/productReducer';
-import chatReducer from './reducers/chatReducer';
 
 const rootReducer = combineReducers({
-  auth: authReducer,
-  product: productReducer,
-  chat: chatReducer,
   // 전역 관리 state가 추가되면 여기에 추가
 });
 
